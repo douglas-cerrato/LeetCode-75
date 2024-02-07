eea95a9e05212da06f57a3a3ae02a9cbc180e6ef
yung bruh <yungbruh@yungbruh>
2024-02-07 00:56:08 +0000

    Created first folder for the LeetCode-75 Questions. Working on TwoSum

---
+++ Arrays/TwoSums.py
@@ -0,0 +1,13 @@
+# TwoSums LeetCode Question
+# https://leetcode.com/problems/two-sum/description/
+
+# Task: 
+# Given an array of integer nums and an integer target, return indices of the two numbers 
+# such that they add up to target.
+
+# Here is an example:
+# nums = [2, 7, 11, 15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0,1].
+
+
