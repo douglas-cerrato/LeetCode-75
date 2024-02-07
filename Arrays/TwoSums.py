cfe512b82efb77d751d307e4bfb96aa1596d5284
yung bruh <douglascerrato2@gmail.com>
2024-02-07 01:33:47 +0000

    Finished TwoSums.py Ran this through LeetCode's test cases and added the test cases within the actual code

--- Arrays/TwoSums.py
+++ Arrays/TwoSums.py
@@ -11,12 +11,24 @@
 # Explanation: Because nums[0] + nums[1] == 9, we return [0,1].
 
 def twoSum(nums, target):
-    return 0
+    for index, number in enumerate(nums):
+        for index2, number2 in enumerate(nums):
+            if((index != index2) and (number+number2 == target)):
+               return [index, index2]
 
 def main():
-    nums = [2,7,11,15]
-    target = 9
 
-    twoSum(nums, target)
+    nums1 = [2,7,11,15]
+    target1 = 9
+    
+    nums2 = [3,2,4]
+    target2 = 6
+
+    nums3 = [3,3]
+    target3 = 6
+
+    print("Test Case 1: {}".format([0,1] == twoSum(nums1, target1)))
+    print("Test Case 2: {}".format([1,2] == twoSum(nums2, target2)))
+    print("Test Case 3: {}".format([0,1] == twoSum(nums3, target3)))
 
 main()
