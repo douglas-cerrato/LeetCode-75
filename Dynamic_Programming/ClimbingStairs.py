b5199c331fb11bbbd8f2385455403238a164881b
yung bruh <douglascerrato2@gmail.com>
2024-02-09 06:15:02 +0000

    ClimbingStairs.py ------------------ Pattern found in how to find and return how many steps can be taken based on N. Seems to have a pattern related to the fibonacci sequence. Going to look  more into it and solve this

--- Dynamic_Programming/ClimbingStairs.py
+++ Dynamic_Programming/ClimbingStairs.py
@@ -34,7 +34,18 @@ def climbStairs(n: int) -> int:
 def main():
     print("Test Case 1: {}".format(climbStairs(2)==2))
     print("Test Case 2: {}".format(climbStairs(3)==3))
-    print("Test Case 3: {}".format(climbStairs(7)==21))
-    print("Test Case 4: {}".format(climbStairs(10)==89))
+    print("Test Case 3: {}".format(climbStairs(4)==5))
+    print("Test Case 4: {}".format(climbStairs(5)==8))
+    print("Test Case 5: {}".format(climbStairs(6)==13))  
+    print("Test Case 6: {}".format(climbStairs(7)==21))
+    print("Test Case 7: {}".format(climbStairs(8)==34))
+    print("Test Case 8: {}".format(climbStairs(7)==55))
+    print("Test Case 9: {}".format(climbStairs(10)==89))
+
+    # NOTICE: I realized after working with the test cases and their expected results that
+    # each amount of distinct ways you can climb to the top for N is equivalent to the index
+    # N holds at the sequence of the fibonacci sequence. I wonter how this correlates, but
+    # it is clear that if I just implement the fibonnaci sequence using N, I can return back 
+    # the possible amount of ways that there can be steps going up stairs
 
 main()
