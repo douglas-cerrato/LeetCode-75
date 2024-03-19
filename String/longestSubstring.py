b11c79baa87d39adc388756ed0bbc8040f9648e9
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-19 21:35:06 +0000

    Initial commit for this branch

---
+++ String/longestSubstring.py
@@ -0,0 +1,7 @@
+def lengthOfLongestSubstring(s: str) -> int:
+    return 0
+
+
+print("Test Case 1: ", lengthOfLongestSubstring("abcabcbb") == 3)
+print("Test Case 2: ", lengthOfLongestSubstring("bbbbb") == 1)
+print("Test Case 3: ", lengthOfLongestSubstring("pwwkew") == 3)
