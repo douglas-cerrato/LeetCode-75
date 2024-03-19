1aaabe5eae8eec17ef5bd31dca9e29170560cfd3
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-19 19:06:01 +0000

    Starting to work on the problem

---
+++ Matrix/setZeroes.py
@@ -0,0 +1,9 @@
+def setZeroes(matrix: list[list[int]]) -> None:
+    pass
+
+# Based on X and Y position, all matching index variables
+# will be turned into 0
+
+Case1 = [[1,1,1],[1,0,1],[1,1,1]]
+Case2 = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+
