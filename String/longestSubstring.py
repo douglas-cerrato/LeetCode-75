6a1d4f138046a5590b2e01e989f10abc20d50579
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-20 00:10:28 +0000

    Completed leetcode problem --------------------------- Completed the leetcode problem, passed all tests in the solutions. There does seem to be a way to have the solution solve in average of 50ms, while this one averages 370ms (within Python3). Might come back to this to look int that

--- String/longestSubstring.py
+++ String/longestSubstring.py
@@ -1,17 +1,27 @@
 def lengthOfLongestSubstring(s: str) -> int:
+    # Declare tempSubstring to use for creating substrings, and longestSubString for comparison
     tempSubstring = ""
     longestSubstring = ""
+    
     for index, x in enumerate(s):
-        for y in range(index, len(s)):      
+        for y in range(index, len(s)):
+
+            # add current variable we are looking at to tempSubstring
+            # if there is no repeat     
             if(s[y] not in tempSubstring):
                 tempSubstring = tempSubstring + s[y]
                 
+                # if longestSubstring is empty, populate with current tempSubstring we have
                 if len(longestSubstring) == 0: 
                     longestSubstring = tempSubstring
+                # if longestSubstring exists and it is smaller then our current tempSubstring
                 elif(len(longestSubstring) < len(tempSubstring)): 
                     longestSubstring = tempSubstring
             else:
+                # We found a duplicate in our tempSubstring
+                # Reset temp to nothing and break out of this loop
                 tempSubstring = ""
+                break
 
     return len(longestSubstring)
 
