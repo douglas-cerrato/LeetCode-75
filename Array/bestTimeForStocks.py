55163966aeb1f27696d3a50cacb34c0e4390b5b2
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-20 19:31:44 +0000

    Finished bestTimeForStocks.py Completed the optimization of this program, and now it runs in average of 715ms when running through the extremely large test cases on leetcode.
    I optimized like this
    ------------------------
    Instead of my previous way of gathering the highestPrice a stock could have
    made money through nesting a for loop to check all possibilitied, I realized
    that I could set a variable to the next lowest value within that list, we can
    attempt to find a higher stock increase gap from there. That works due to the fact
    that we will only lower our lowCount value when the days stock is lower then the
    current stock, so if it is higher we are increasing our highestPrice already.
    
    This method makes it so that we can find the best day with only one iteration
    of the prices list instead of a nested iteration.

--- Array/bestTimeForStocks.py
+++ Array/bestTimeForStocks.py
@@ -1,11 +1,20 @@
 def maxProfit(prices: list[int]) -> int:
     highestPrice = 0
+    lowCount = None
     for indexX, x in enumerate(prices):
-        for y in range(indexX+1, len(prices)):
-            if (x < prices[y] and (prices[y] - x) > highestPrice):
-                highestPrice = prices[y] - x
+        
+        if(lowCount == None):
+            lowCount = indexX
+            continue
+
+        if(lowCount != indexX):
+            if(x < prices[lowCount]):
+                lowCount = indexX
+
+        if((x - prices[lowCount]) > highestPrice):
+            highestPrice =  x - prices[lowCount]
     
     return highestPrice
 
-print("Test Case 1", maxProfit([7,1,5,3,6,4]) == 5)
-print("Test Case 2", maxProfit([7,6,4,3,1]) == 0)
+print("Test Case 1: ", maxProfit([7,1,5,3,6,4]) == 5)
+print("Test Case 2: ", maxProfit([7,6,4,3,1]) == 0)
