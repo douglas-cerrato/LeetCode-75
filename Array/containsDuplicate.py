5042ec590f828a47e60b0241c0e03ca359b33514
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-21 21:12:07 +0000

    finished containsDuplicate.py, kinda... Finished the program and it works, however it exceeds time limit for leetcodes test problems with larger data sets. Working on optimization

--- Array/containsDuplicate.py
+++ Array/containsDuplicate.py
@@ -1,5 +1,11 @@
-def containsDuplicate(self, nums: list[int]) -> bool:
-    return False
+def containsDuplicate(nums: list[int]) -> bool:
+        for indexX, x in enumerate(nums):
+            newNums = nums[indexX+1:]
+            print(f"newNums is {newNums}")
+            if(x in newNums):
+                return True
+        
+        return False
 
 
 print("Test Case 1: ", containsDuplicate([1,2,3,1])==True)
