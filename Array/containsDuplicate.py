b3f41c9f2e11a43c8758b0d9eca4c99d2e0222d0
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-21 12:03:20 -0700

    Init created for containsDuplicate.py

---
+++ Array/containsDuplicate.py
@@ -0,0 +1,7 @@
+def containsDuplicate(self, nums: list[int]) -> bool:
+    return False
+
+
+print("Test Case 1: ", containsDuplicate([1,2,3,1])==True)
+print("Test Case 2: ", containsDuplicate([1,2,3,4])==False)
+print("Test Case 3: ", containsDuplicate([1,1,1,3,3,4,3,2,4,2])==True)
